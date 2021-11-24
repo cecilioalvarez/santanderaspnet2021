29df9054359c0f6150dd2993dbc73916574fa32a
cecilioAlvarez <cmac@arquitecturajava.com>
2021-11-24 09:23:13 +0100

    nuevos

---
+++ Controllers/FacturaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using net.Models;
+
+namespace net.Controllers
+{
+    public class FacturaController : Controller
+    {
+
+        public FacturaController()
+        {
+           
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RecibirDatosFactura(Factura factura) {
+
+            ViewBag.Numero=factura.Numero;
+            ViewBag.Concepto=factura.Concepto;
+            ViewBag.Importe=factura.Importe;
+            return View();
+        }
+
+         public IActionResult FormularioFactura()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Models/Factura.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace net.Models
+{
+    public class Factura
+    {
+        public int Numero { get; set; }
+        public string Concepto { get; set; }
+        public double Importe { get; set; }
+
+    }
+}
